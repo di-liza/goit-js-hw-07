38551c3267437cd6faa5a85334a7065c7a36c964
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-03-06 15:29:57 +0200

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.innerHTML = galleryItemsMarkup;
+
+function createGalleryItemsMarkup(galleryItems) {
+  const makeGallery = galleryItems
+    .map((item) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+  return makeGallery;
+}
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  const prewievImage = event.target.nodeName === "IMG";
+  if (!prewievImage) {
+    return;
+  }
+  showLargeGalleryImage(event);
+}
+
+let galleryModal = null;
+
+function showLargeGalleryImage({ target }) {
+  const largeImage = target.dataset.source;
+
+  galleryModal = basicLightbox.create(
+    ` <img
+        src="${largeImage}"
+      />`
+  );
+  document.addEventListener("keydown", onEscapeKeyDown);
+  galleryModal.show();
+}
+
+function closeGalleryModal() {
+  document.removeEventListener("keydown", onEscapeKeyDown);
+  galleryModal.close();
+}
+function onEscapeKeyDown(event) {
+  if (event.key === "Escape") {
+    closeGalleryModal();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector(".gallery");
+
+const makeGalleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryListEl.innerHTML = makeGalleryItemsMarkup;
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map((item) => {
+      return `<li><a class="gallery__item lightbox" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></li>`;
+    })
+    .join("");
+}
+
+const makeSimpleLightBoxGallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
